fe54e1d8ecfa41ef007b1037aa308639d8234e4d
NirmalShah <nirmalshah20519@gmail.com>
2021-06-30 21:05:55 +0530

    30/06/19(1)

--- ADA/PRACTICAL_3/max_heap_sort.py
+++ ADA/PRACTICAL_3/max_heap_sort.py
@@ -1,35 +1,31 @@
-# Max-Heap Sort
+from time import *
 
-def max_heapify(Arr,i):
-    n=len(Arr)
-    L=Arr[2*i]
-    R=Arr[2*i+1]
-    if L<=n and Arr[L]>Arr[i]:
-        Largest=L
-    else:
-        Largest=i
-    if R<n and Arr[R]>Arr[Largest]:
-        Largest=R
-    if Largest!=i:
-        Arr[i],Arr[Largest]=Arr[Largest],Arr[i]
-        max_heapify(Arr,Largest)
+def Heapify(arr,n,i):
+    L = i
+    left=2*i+1
+    right=2*i+2
+    if left<n and arr[i]<arr[left]:
+        L=left
+    if right<n and arr[L]<arr[right]:
+        L=right
+    if L!=i:
+        arr[i],arr[L]=arr[L],arr[i]
+        Heapify(arr,n,L)
+  
+def heapSort(arr):
+    n=len(arr)
+    for i in range(n//2,-1,-1):
+        Heapify(arr, n, i)
+  
+    for i in range(n-1, 0, -1):
+        arr[i], arr[0] = arr[0], arr[i]
+        Heapify(arr, i, 0)
+  
+arr = [58,96,14,23,74,52,49,37,82,99,9,1,28,77,29]
+t=time()
+heapSort(arr)
+n = len(arr)
+print(arr)
+print("Max-Heap sort completed in ",time()-t," sec")
 
-
-
-def build_max_heap(Arr):
-    n=len(Arr)
-    for i in range((n//2),0):
-        max_heapify(Arr,i)
-
-
-def heap_sort(Arr):
-    n=len(Arr)
-    build_max_heap(Arr)
-    for i in range(n,1):
-        Arr[1],Arr[i]=Arr[i],Arr[1]
-        n=n-1
-        max_heapify(Arr,1)
-
-L1=[9,5,7,12,2,6,4]
-heap_sort(L1)
-print(L1)
+  
